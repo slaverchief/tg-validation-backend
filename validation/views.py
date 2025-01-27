e67430a9a30f09a1e908fc4077f26b23dd9d77d3
slaverchief <uvaysov.2005@mail.ru>
2025-01-27 22:21:35 +0300

    User validation

--- validation/views.py
+++ validation/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+import json
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse
+from .models import TGuser
 
-# Create your views here.
+def check_user(request):
+    user = json.loads(request.body.decode('utf-8')).get('user')
+    try:
+        TGuser.objects.get(tgid=user)
+        return HttpResponse(status=200)
+    except ObjectDoesNotExist:
+        return HttpResponse(status=404)
