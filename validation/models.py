44cdb65293d6916ac38aa93e0dde952b328c4c23
slaverchief <uvaysov.2005@mail.ru>
2025-01-27 22:10:36 +0300

    DB index for user

--- validation/models.py
+++ validation/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class TGuser(models.Model):
-    tgid = models.CharField(max_length=100)
+    tgid = models.CharField(max_length=100, unique=True, db_index=True)
 
     class Meta:
         verbose_name_plural = "Кто власть имеет"
