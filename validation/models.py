dd8175158d1d6253d4a10ded6f8264e485477420
slaverchief <uvaysov.2005@mail.ru>
2025-01-27 22:06:19 +0300

    TG user model

--- validation/models.py
+++ validation/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class TGuser(models.Model):
+    tgid = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "Кто власть имеет"
+        verbose_name = verbose_name_plural
