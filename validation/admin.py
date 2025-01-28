5e3b1b3b591aa7425a7aef7526be1e5cd462f599
slaverchief <uvaysov.2005@mail.ru>
2025-01-28 23:56:28 +0300

    Grouping chats

--- validation/admin.py
+++ validation/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(TGuser)
-admin.site.register(Groups)
+admin.site.register(Group)
+admin.site.register(Chat)
--- validation/models.py
+++ validation/models.py
@@ -7,9 +7,25 @@ class TGuser(models.Model):
         verbose_name_plural = "Кто власть имеет"
         verbose_name = verbose_name_plural
 
-class Groups(models.Model):
-    tgid = models.CharField(max_length=100, unique=True, db_index=True)
+class Chat(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    chatid = models.CharField(max_length=100, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Чаты"
+        verbose_name = "Чат"
+
+class Group(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    chats = models.ManyToManyField(Chat)
+
+    def __str__(self):
+        return self.name
+
 
     class Meta:
         verbose_name_plural = "Группы"
-        verbose_name = "group"
+        verbose_name = "Группа"
